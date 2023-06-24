7bed428df2cf0fa27e609bd8bd8325c4adb580bc
kunal nayak <87607403+Kunal766@users.noreply.github.com>
2023-06-24 19:59:43 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -75,7 +75,7 @@ def predict(data: str):
 
     # # 5. Run the API with uvicorn
     # #    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='127.0.0.1', port=8000)
 
 # #uvicorn app:app --reload
